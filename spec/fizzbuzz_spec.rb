18d61cc477dd6cb561f02fa6cce4455b50013200
Raven Sears <ravenrosereynolds@gmail.com>
2021-12-07 22:14:04 +0000

    add more tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,8 +1,38 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
+    it 'returns "1" when passed 1' do 
+        expect(fizzbuzz(1)).to eq 1
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns 263 when passed 11' do
+        expect(fizzbuzz(11)).to eq 11
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns 263 when passed 263' do
+        expect(fizzbuzz(263)).to eq 263
+    end
+end
+
+describe 'fizzbuzz' do
     it 'returns "fizz" when passed 3' do
         expect(fizzbuzz(3)).to eq 'fizz'
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns "fizz" when passed 63' do
+        expect(fizzbuzz(63)).to eq 'fizz'
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns "fizz" when passed 996' do
+        expect(fizzbuzz(996)).to eq 'fizz'
     end
 end
 
@@ -13,7 +43,31 @@ describe 'fizzbuzz' do
 end
 
 describe 'fizzbuzz' do
+    it 'returns "buzz" when passed 85' do
+        expect(fizzbuzz(85)).to eq 'buzz'
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns "buzz" when passed 445' do
+        expect(fizzbuzz(445)).to eq 'buzz'
+    end
+end
+
+describe 'fizzbuzz' do
     it 'returns "fizzbuzz" when passed 15' do
         expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+end
+
+describe 'fizzbuzz' do 
+    it 'returns "fizzbuzz" when passed 90' do
+        expect(fizzbuzz(90)).to eq 'fizzbuzz'
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns "fizzbuzz" when passed 330' do
+        expect(fizzbuzz(330)).to eq 'fizzbuzz'
     end
 end
